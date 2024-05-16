67e765dd1a6ab11ba8361ce4820837ef6ec7c367
TheanoG <theano.goeman@student.hogent.be>
2024-05-16 13:54:20 +0200

    Code nginx

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,6 +1,8 @@
 docker pull nginx
 
-docker run --name my-nginx -v html:/usr/share/nginx/html:ro -d nginx
+icacls .\html\* /grant "IIS_IUSRS:(OI)(CI)R"
 
+docker run --name my-nginx -v html:/usr/share/nginx/html:ro -d -p 81:80 nginx
 
-docker run my-nginx -d -p 81:80 
+docker restart my-nginx
+
